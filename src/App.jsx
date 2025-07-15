bcf3bf5f7a88b62655a689ae812801b67b9a1c32
Nicholas <nicholaskuokjinshumg@gmail.com>
2025-07-15 13:46:43 +0800

    exer3

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,10 @@ function App() {
   const [email, setEmail] = useState("");
   const [subscribed, setSubscribed] = useState(false);
 
+  const clear = () => {
+    setName(""), setEmail(""), setSubscribed(false);
+  };
+
   return (
     <>
       <div className="container mt-5">
@@ -41,10 +45,15 @@ function App() {
           </form>
         ) : (
           <div
-            className="alert alert-success d-flex align-items-center mt-3"
+            className="alert alert-success d-flex align-items-center mt-3 flex-column"
             role="alert"
           >
             <span>Please check your email ( {email} )for confirmation.</span>
+            <div className="pt-3">
+              <button className="btn btn-warning" onClick={clear}>
+                Subscribe to another email
+              </button>
+            </div>
           </div>
         )}
       </div>
